2df80c2fc743680ad6b03ac1c68798311c72efba
Nicolas Moreau <nicolas.moreau93@orange.fr>
2022-01-05 14:27:03 +0100

    1 commit connexion a la bdd : test jpa

---
+++ src/main/java/dev/Pizza.java
@@ -0,0 +1,33 @@
+package dev;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Pizza {
+	
+	@Id
+	private Integer id;
+	private String name;
+	private String size;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+}
---
+++ src/main/java/dev/TestJpa.java
@@ -0,0 +1,24 @@
+package dev;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("testjpa");
+		
+		EntityManager em = emf.createEntityManager();
+			
+//		Pizza pizza = em.find(Pizza.class, 3);
+//		System.out.println("Pizza size=" + pizza.getSize());
+
+		em.close();
+
+		emf.close();
+		
+	}
+
+}
